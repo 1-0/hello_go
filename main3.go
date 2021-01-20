4cb28ae5fec75220d398914e7230ddfd8a0293eb
1-0 <1_0@usa.com>
2021-01-20 04:29:18 +0200

    Fix in repo files

--- main3.go
+++ main3.go
@@ -1,8 +1,44 @@
 package main
+ 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
 
-import "fmt"
-
-func main() {
-	fmt.Println("Hello, NIX Education")
-	fmt.Println("Hello, NIX Education")
+// simpley http client
+func client_http(req_url, req_type string) string {
+	req, err := http.NewRequest(req_type, req_url, nil)
+	if err != nil {
+		log.Fatal("Error reading request. ", err)
+	}
+ 
+	req.Header.Set("Cache-Control", "no-cache")
+ 
+	client := &http.Client{Timeout: time.Second * 10}
+ 
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error reading response. ", err)
+	}
+	defer resp.Body.Close()
+ 
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading body. ", err)
+	}
+	return string(body)
 }
+ 
+func main() {
+	const base_url = "https://jsonplaceholder.typicode.com/"
+	var res string
+	url := base_url + "posts"
+	r_type := "GET"
+	res = client_http(url, r_type)
+
+	fmt.Printf("%s\n", res)
+}
+
