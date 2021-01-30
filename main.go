9d956a2fa8753d320774139768b8edb2a5d3ffa5
1-0 <1_0@usa.com>
2021-01-30 16:31:29 +0200

    Fix in gorouting running

--- main.go
+++ main.go
@@ -12,40 +12,52 @@ import (
 )
 
 /* type post struct {
-	UserId int     `json:"userId"`
-	Id  int           `json:"id"`
-	Title string  `json:"title"`
-	Body string `json:"body"`
+	UserId int   `json:"userId"`
+	Id  int      `json:"id"`
+	Title string `json:"title"`
+	Body string  `json:"body"`
 } */
 
+// simpley write post
+func writePost(post []byte, path string, id int) {
+	//message := []byte("Hello, Gophers!")
+	var newFile = path + strconv.Itoa(id)
+	err := ioutil.WriteFile(newFile, post, 0644)
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	fmt.Println("newFile - ", newFile)
+}
+
 // simpley get and print post
-func printPost(reqURL string, id int) {
+func getPost(reqURL string, id int) {
+	var writePath = "./storage/posts/"
 
 	var url = reqURL + strconv.Itoa(id)
-	fmt.Println("url:", url)
+	// fmt.Println("url:", url)
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Error reading request. ", err)
 		log.Fatal("Error reading request. ", err)
 	}
 	defer resp.Body.Close()
-	// defer resp.Close()
-	// fmt.Println("url:", url)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Error reading response. ", err)
 		log.Fatal("Error reading response. ", err)
 	}
-	fmt.Println(string(body))
-	amt := time.Duration(rand.Intn(250))
-	time.Sleep(time.Millisecond * amt)
-	// time.Sleep(time.Second * 1)
+	writePost(body, writePath, id)
+	// fmt.Println(string(body))
 }
 
 func main() {
 	const baseURL = "https://jsonplaceholder.typicode.com/"
 	url := baseURL + "posts/"
+	//_ = os.MkdirAll("./storage/posts/", 0770)
 	for i := 1; i <= 100; i++ {
-		printPost(url, i)
+		go getPost(url, i)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
 	}
+	amt2 := time.Duration(rand.Intn(20))
+	time.Sleep(time.Second * amt2)
 }
--- main4.go
+++ main4.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
+	// "log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -21,31 +21,28 @@ import (
 // simpley get and print post
 func printPost(reqURL string, id int) {
 
-	var url = reqURL + strconv.Itoa(id)
-	fmt.Println("url:", url)
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error reading request. ", err)
-		log.Fatal("Error reading request. ", err)
+	var NewURL = reqURL + strconv.Itoa(id)
+	fmt.Println("url:", NewURL)
+	resp, err1 := http.Get(NewURL)
+	if err1 != nil {
+		fmt.Println("Error reading request. ", err1)
 	}
 	defer resp.Body.Close()
-	// defer resp.Close()
-	// fmt.Println("url:", url)
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response. ", err)
-		log.Fatal("Error reading response. ", err)
+	var body, err2 = ioutil.ReadAll(resp.Body)
+	if err2 != nil {
+		fmt.Println("Error reading response. ", err2)
 	}
-	fmt.Println(string(body))
-	amt := time.Duration(rand.Intn(250))
-	time.Sleep(time.Millisecond * amt)
-	// time.Sleep(time.Second * 1)
+    fmt.Println(string(body))
 }
 
 func main() {
 	const baseURL = "https://jsonplaceholder.typicode.com/"
 	url := baseURL + "posts/"
 	for i := 1; i <= 100; i++ {
-		printPost(url, i)
+		go printPost(url, i)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
 	}
+	amt2 := time.Duration(rand.Intn(20))
+	time.Sleep(time.Second * amt2*5)
 }
