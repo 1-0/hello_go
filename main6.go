6cc7cabe6d08ed799ca805b4034fc65133d6db68
1-0 <1_0@usa.com>
2021-02-01 08:29:52 +0200

    Added interface saveToDB

--- main6.go
+++ main6.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -13,11 +13,44 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+const baseURL = "https://jsonplaceholder.typicode.com/"
+
+
+// simpley save to DB record
+var i interface {
+    saveToDB()
+}
+
 type post struct {
 	UserID int   `json:"userId"`
 	ID int      `json:"id"`
 	Title string `json:"title"`
 	Body string  `json:"body"`
+}
+
+func (p post) saveToDB() {
+	db, err := sql.Open("mysql",
+		"root:2w2w2w2w2w@tcp(127.0.0.1:3306)/hello_go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt, err := db.Prepare("INSERT INTO posts(UserID, ID, Title, Body) VALUES(?, ?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := stmt.Exec(p.UserID, p.ID, p.Title, p.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lastId, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rowCnt, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("insertPost %d ID = %d, affected = %d\n", p.ID, lastId, rowCnt)
 }
 
 type comment struct {
@@ -28,67 +61,17 @@ type comment struct {
 	Body string  `json:"body"`
 }
 
-// simpley write post to DB
-func insertPost(newPost post) {
+func (c comment) saveToDB() {
 	db, err := sql.Open("mysql",
 		"root:2w2w2w2w2w@tcp(127.0.0.1:3306)/hello_go")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Println("db ", db)
-	stmt, err := db.Prepare("INSERT INTO posts(UserID, ID, Title, Body) VALUES(?, ?, ?, ?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	res, err := stmt.Exec(newPost.UserID, newPost.ID, newPost.Title, newPost.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	lastId, err := res.LastInsertId()
-	if err != nil {
-		log.Fatal(err)
-	}
-	rowCnt, err := res.RowsAffected()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("insertPost %d ID = %d, affected = %d\n", newPost.ID, lastId, rowCnt)
-	// var (
-	// 	id int
-	// 	name string
-	// )
-	// rows, err := db.Query("select id from post where id = ?", 1)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer rows.Close()
-	// for rows.Next() {
-	// 	err := rows.Scan(&id, &name)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	log.Println(id, name)
-	// }
-	// err = rows.Err()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// 	defer db.Close()
-	// }
-}
-
-// simpley write comment to DB
-func insertComment(newComments comment) {
-	db, err := sql.Open("mysql",
-		"root:2w2w2w2w2w@tcp(127.0.0.1:3306)/hello_go")
-	if err != nil {
-		log.Fatal(err)
-	}
-	// fmt.Println("db ", db)
 	stmt, err := db.Prepare("INSERT INTO comments(PostID, ID, Name, Email, Body) VALUES(?, ?, ?, ?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
-	res, err := stmt.Exec(newComments.PostID, newComments.ID, newComments.Name, newComments.Email, newComments.Body)
+	res, err := stmt.Exec(c.PostID, c.ID, c.Name, c.Email, c.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -100,7 +83,7 @@ func insertComment(newComments comment) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("newComments %d ID = %d, affected = %d\n", newComments.ID, lastId, rowCnt)
+	log.Printf("newComments %d ID = %d, affected = %d\n", c.ID, lastId, rowCnt)
 }
 
 // simpley get posts comments and print post
@@ -119,7 +102,7 @@ func getPostComments(reqURL string, id int) {
 	json.Unmarshal(body, &comments)
 	for c := range comments{
 		// fmt.Println("comments[c].ID ", comments[c].ID)
-		go insertComment(comments[c])
+		go comments[c].saveToDB()
 		amt := time.Duration(rand.Intn(250))
 		time.Sleep(time.Millisecond * amt)
 	}
@@ -127,7 +110,6 @@ func getPostComments(reqURL string, id int) {
 
 // simpley get user posts id and print post
 func getUserPosts(reqURL string, id int) {
-	const baseURL = "https://jsonplaceholder.typicode.com/"
 	var url = reqURL + strconv.Itoa(id)
 	resp, err1 := http.Get(url)
 	if err1 != nil {
@@ -142,19 +124,19 @@ func getUserPosts(reqURL string, id int) {
 	json.Unmarshal(body, &posts)
 	url2 := baseURL + "comments?postId="
 	for i := range posts {
-		go insertPost(posts[i])
-		go getPostComments(url2, posts[i].ID)
+		go posts[i].saveToDB()
 		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+		go getPostComments(url2, posts[i].ID)
 		time.Sleep(time.Millisecond * amt)
 	}	
 }
 
 func main() {
-	const baseURL = "https://jsonplaceholder.typicode.com/"
 	userID := 7
 	url := baseURL + "posts?userId="
 	go getUserPosts(url, userID)
 	amt2 := time.Duration(5)
 	time.Sleep(time.Second * amt2)
-	fmt.Println("-----------------------exit--------------------")
+	// fmt.Println("-----------------------exit--------------------")
 }
